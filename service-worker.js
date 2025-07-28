53077fc73d773def79f97a9ae3abda6177fd950a
slide-t <slideteckacademy@gmail.com>
2025-07-28 20:17:31 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,59 @@
+const CACHE_NAME = "iolgregister-v1.1.3";
+const urlsToCache = [
+  "https://slide-t.github.io/iolgregister/",
+  "https://slide-t.github.io/iolgregister/index.html",
+  "https://slide-t.github.io/iolgregister/main.html",
+  "https://slide-t.github.io/iolgregister/style.css",
+  "https://slide-t.github.io/iolgregister/script.js",
+  "https://fonts.googleapis.com/css2?family=Roboto&display=swap",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) return caches.delete(cache);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).catch(() =>
+          caches.match("https://slide-t.github.io/iolgregister/index.html")
+        )
+      );
+    })
+  );
+});
+
+
+
+
+
+
+
+/*
 const CACHE_NAME = 'iolg-cache-v1.1.2';
 const urlsToCache = [
   'index.html',
