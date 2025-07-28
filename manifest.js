6f7726e7a100f63f03fc76f4ca17941aa3111305
slide-t <slideteckacademy@gmail.com>
2025-07-28 19:28:24 +0100

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,22 @@
+{
+  "name": "IOLG Register",
+  "short_name": "IOLG",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#004080",
+  "orientation": "portrait",
+  "description": "Offline Voter Registration Tool for IOLG",
+  "icons": [
+    {
+      "src": "icons/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
