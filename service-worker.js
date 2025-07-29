9a4ce58b56365a40f4f585ebe3b34c06c6db263c
slide-t <slideteckacademy@gmail.com>
2025-07-29 05:29:45 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "iolgregister-v2.3.3";
+const CACHE_NAME = "iolgregister-v2.3.4";
 const urlsToCache = [
   "https://slide-t.github.io/iolgregister/",
   "https://slide-t.github.io/iolgregister/index.html",
