4eaafb8d90e8870457d94aa480b785cbc2493cb3
slide-t <slideteckacademy@gmail.com>
2025-07-29 05:27:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ setInterval(function () {
   };
   console.log("%c", devtools);
 }, 1000);
+
+if (window.outerWidth - window.innerWidth > 100) {
+  alert("Developer tools are open. Access denied.");
+  window.location.href = "about:blank";
+}
